7b5888aaa0a7f70759cf20a3a10e626900d8308f
LuisaMaria21 <88382889+LuisaMaria21@users.noreply.github.com>
2023-04-20 17:19:10 -0500

    Cambios casi finales :OOO

--- Parcial2-Luisa/src/Posts.jsx
+++ Parcial2-Luisa/src/Posts.jsx
@@ -10,7 +10,7 @@ const Posts = () => {
 
   useEffect(() => {
     dispatch(fetchPosts());
-  }, [dispatch]);
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -22,7 +22,7 @@ const Posts = () => {
 
   return (
     <div>
-      {posts.map((post) => (
+      {posts?.map((post) => (
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.body}</p>

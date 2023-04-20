bcff4807b3a4e6b158f5ce7db01105da33d849da
LuisaMaria21 <88382889+LuisaMaria21@users.noreply.github.com>
2023-04-20 17:19:36 -0500

    cambios casi finales :0

--- Parcial2-Luisa/src/redux/actions.js
+++ Parcial2-Luisa/src/redux/actions.js
@@ -1,31 +1,16 @@
-export const FETCH_POSTS_REQUEST = 'FETCH_POSTS_REQUEST';
-export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
-export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
+import { setPost } from "./postsSlice";
 
-export const fetchPostsRequest = () => ({
-  type: FETCH_POSTS_REQUEST,
-});
-
-export const fetchPostsSuccess = (posts) => ({
-  type: FETCH_POSTS_SUCCESS,
-  payload: posts,
-});
-
-export const fetchPostsFailure = (error) => ({
-  type: FETCH_POSTS_FAILURE,
-  payload: error,
-});
 
 export const fetchPosts = () => {
-  return (dispatch) => {
-    dispatch(fetchPostsRequest());
-    return fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch(fetchPostsSuccess(data));
-      })
-      .catch((error) => {
-        dispatch(fetchPostsFailure(error));
-      });
-  };
+    return async (dispatch) => {
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((data) => {
+                dispatch(setPost(data));
+            })
+            .catch((error) => {
+                dispatch(fetchPostsFailure(error));
+            });
+    };
 };
--- Parcial2-Luisa/src/redux/postsSlice.js
+++ Parcial2-Luisa/src/redux/postsSlice.js
@@ -3,16 +3,16 @@ import { fetchPosts } from './thunks';
 
 const postsSlice = createSlice({
     name: 'posts',
-    initialState: [],
-    reducers: {},
-    extraReducers: (builder) => {
-      builder.addCase(fetchPosts.fulfilled, (state, action) => {
-        return action.payload;
-      });
+    initialState: {posts:[]},
+    reducers: {
+      setPost:(state,action)=>{
+        state.posts = action.payload
+      }
     },
+    
   });
   
-  export const { actions } = postsSlice;
+  export const { setPost } = postsSlice.actions;
   export default postsSlice.reducer;
 
 
